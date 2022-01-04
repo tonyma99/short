46f072b534abc28d652e94ef4fa4b9a1c67f40e1
Tony Ma <tonyphma@gmail.com>
2022-01-04 15:01:09 -0800

    Add click counter for links (#3)

--- pages/[redirect].tsx
+++ pages/[redirect].tsx
@@ -5,11 +5,15 @@ export default function Redirect() { }
 
 export const getServerSideProps: GetServerSideProps = async context => {
     const { redirect } = context.query
-    const { db } = await connectToDatabase();
+    const { db } = await connectToDatabase()
 
     const data = await db.collection('links').findOne({ shortUrl: redirect })
     
     if (data) {
+        await db.collection('links').updateOne(
+            { shortUrl: redirect },
+            { $inc: {count: 1} }
+        )
         return {
             redirect: {
                 destination: data.fullUrl,
--- pages/api/shorten.ts
+++ pages/api/shorten.ts
@@ -23,7 +23,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     
     await db.collection('links').insertOne({
         fullUrl: fullUrl,
-        shortUrl: shortUrl
+        shortUrl: shortUrl,
+        count: 0
     })
 
     return res.json({
