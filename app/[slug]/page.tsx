8a3a12b14d71baa06456513e4819f5727ccb0970
Tony Ma <tonyphma@gmail.com>
2023-03-05 00:22:40 -0800

    Handle invalid links

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import { headers } from 'next/headers'
+import { redirect } from 'next/navigation'
 import styles from './page.module.css'
 import RedirectModal from './RedirectModal'
 
@@ -6,15 +7,18 @@ const getUrl = async (id: string) => {
 	const headersList = headers()
 	const protocol = process.env.NODE_ENV === 'development' ? 'http' : 'https'
 	const host = headersList.get('host')
-	console.log(`${protocol}://${host}/api/expand?id=${id}`)
 	const response = await fetch(`${protocol}://${host}/api/expand?id=${id}`, { cache: 'no-store' })
-	const { url } = await response.json()
-	return url
+	if (response.ok) {
+		const { url } = await response.json()
+		return url
+	}
 }
 
 export default async function Redirect({ params }: { params: any }) {
 	const { slug: id } = params
 	const url = await getUrl(id)
+
+	if (!url) throw redirect('/')
 
 	return (
 		<div className={styles.modalContainer}>
