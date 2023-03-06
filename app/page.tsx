5cabf92c7c2f81ff56e2c8df3271423e83bde516
Tony Ma <tonyphma@gmail.com>
2023-03-06 13:37:27 -0800

    Change copy

--- app/page.tsx
+++ app/page.tsx
@@ -6,19 +6,19 @@ export default function Home() {
 		<main className={styles.main}>
 			<div className={styles.description}>
 				<p>
-					Currently a WIP, see version 1&nbsp;
-					<a href="https://short-git-v1-tonyma.vercel.app">here</a>.
+					Work in progress, see version 1&nbsp;
+					<a href="https://short-git-v1-tonyma.vercel.app">here</a>
 				</p>
 				<div>
-					<a href="https://github.com/tonyma99/short" target="_blank" rel="noopener noreferrer">
-						GitHub
-					</a>
 					<a
 						href="https://tonyma.notion.site/Short-ce453964971d4d288b83a194b8e35be3"
 						target="_blank"
 						rel="noopener noreferrer"
 					>
-						Notion
+						Features
+					</a>
+					<a href="https://github.com/tonyma99/short" target="_blank" rel="noopener noreferrer">
+						GitHub
 					</a>
 				</div>
 			</div>
