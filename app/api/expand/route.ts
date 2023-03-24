2831df080ac7076b58286aab4b88023b90870816
Tony Ma <tonyphma@gmail.com>
2023-03-23 21:07:12 -0700

    Use request headers

--- app/api/expand/route.ts
+++ app/api/expand/route.ts
@@ -1,17 +1,15 @@
 import { Links } from '@lib/db'
-import { headers } from 'next/headers'
 
 export async function GET(request: Request) {
 	try {
 		const { searchParams } = new URL(request.url)
 		const id = searchParams.get('id') as string
-
-		const headersList = headers()
+		const { headers } = request
 
 		const url = await Links.get(id)
 
 		if (url) {
-			await Links.update(id, { headers: headersList })
+			await Links.update(id, { headers })
 			return new Response(JSON.stringify({ url }))
 		} else {
 			return new Response('The specified code is invalid.', { status: 400 })
